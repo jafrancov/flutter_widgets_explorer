82dea44cdd3f64f59f48bca8818bb034cc03f32c
Alejandro Franco <jafrancov@gmail.com>
2020-07-11 13:31:53 -0500

    Part 2

--- lib/src/screens/action_sheets_screen.dart
+++ lib/src/screens/action_sheets_screen.dart
@@ -1,26 +1,101 @@
 import 'package:flutter/cupertino.dart';
 
 class ActionSheetScreen extends StatefulWidget {
-  ActionSheetScreen({Key key}) : super(key: key);
-
   @override
-  _ActionSheetScreenState createState() => _ActionSheetScreenState();
+  State<StatefulWidget> createState() {
+    return _ActionSheetScreenState();
+  }
 }
 
 class _ActionSheetScreenState extends State<ActionSheetScreen> {
+  String _selectedValue;
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
         navigationBar: CupertinoNavigationBar(
-          middle: Text('Action Sheets and alerts'),
+          middle: Text('Action Sheet and Alerts'),
           previousPageTitle: 'Widgets',
         ),
         child: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CupertinoButton.filled(child: Text('Alert'), onPressed: () {})
+            child: Center(
+                child: Column(
+          children: <Widget>[
+            SizedBox(height: 50.0),
+            CupertinoButton.filled(
+              padding: EdgeInsets.symmetric(horizontal: 100.0, vertical: 10.0),
+              child: Text('Alert'),
+              onPressed: () => _showAlert(context),
+            ),
+            SizedBox(height: 16.0),
+            CupertinoButton.filled(
+              padding: EdgeInsets.symmetric(horizontal: 70.0, vertical: 10.0),
+              child: Text('Action Sheet'),
+              onPressed: () => _showActionSheet(context),
+            ),
+            SizedBox(height: 16.0),
+            Text('Has seleccionaro $_selectedValue'),
+          ],
+        ))));
+  }
+
+  void _showAlert(BuildContext context) {
+    showCupertinoDialog<String>(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: Text('Guarda los cambios'),
+            content: Text('¿Desea guardar los cambios realizados?'),
+            actions: <Widget>[
+              CupertinoDialogAction(
+                child: Text('Cancelar'),
+                onPressed: () => Navigator.pop(context, 'Cancelar'),
+              ),
+              CupertinoDialogAction(
+                child: Text('Aceptar'),
+                onPressed: () => Navigator.pop(context, 'Aceptar'),
+              )
             ],
-          ),
-        ));
+          );
+        }).then((value) => {
+          setState(() {
+            _selectedValue = value;
+          })
+    });
+  }
+
+  void _showActionSheet(BuildContext context) {
+    showCupertinoModalPopup<String>(
+        context: context,
+        builder: (context) {
+          return CupertinoActionSheet(
+            title: Text('Framework favorito'),
+            message: Text('Selecciona tu framwork multiplataforma favorito'),
+            actions: <Widget>[
+              CupertinoActionSheetAction(
+                child: Text('Flutter'),
+//                isDestructiveAction: true,
+                onPressed: () => Navigator.pop(context, 'Flutter'),
+              ),
+              CupertinoActionSheetAction(
+                child: Text('React Native'),
+                onPressed: () => Navigator.pop(context, 'React Native'),
+              ),
+              CupertinoActionSheetAction(
+                child: Text('Native Script'),
+                onPressed: () => Navigator.pop(context, 'Native Script'),
+              ),
+            ],
+            cancelButton:CupertinoActionSheetAction(
+              child: Text('Cancelar'),
+              isDefaultAction: true,
+              onPressed: () => Navigator.pop(context, 'Cancelar'),
+            ),
+          );
+        }).then((value) => {
+      setState(() {
+        _selectedValue = value;
+      })
+    });
   }
 }
--- lib/src/screens/card_screen.dart
+++ lib/src/screens/card_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
   CardScreen({Key key}) : super(key: key);
@@ -6,14 +7,85 @@ class CardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Cards'),
-      ),
-      child: Center(
-        child: Container(
-          child: Text('Cards'),
+        child: CustomScrollView(
+      slivers: <Widget>[
+        CupertinoSliverNavigationBar(
+          largeTitle: Text('Cards'),
         ),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          _cardTipo1(),
+          _cardtipo2(context),
+          _cardTipo1(),
+          _cardtipo2(context),
+          _cardTipo1(),
+          _cardtipo2(context),
+          _cardTipo1(),
+          _cardtipo2(context),
+        ]))
+      ],
+    ));
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      margin: EdgeInsets.all(20.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      elevation: 15.0,
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(CupertinoIcons.photo_camera),
+            title: Text('This is a card title'),
+            subtitle: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'),
+          )
+        ],
       ),
+    );
+  }
+
+  Widget _cardtipo2(BuildContext context) {
+    final _card = Stack(alignment: Alignment.bottomCenter, children: <Widget>[
+      FadeInImage(
+        placeholder: AssetImage('assets/cupertino_activity_indicator.gif'),
+        image: NetworkImage(
+            'https://www.nationalgeographic.com/content/dam/travel/rights-exempt/2019-travel-photo-contest/epic-landscapes/2019-travel-photo-contest-epic-landscapes031.jpg'),
+        fadeOutDuration: Duration(milliseconds: 200),
+        fit: BoxFit.cover,
+      ),
+      Opacity(
+          opacity: 0.7,
+          child: Container(
+            color: CupertinoColors.black,
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+            height: 80.0,
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('2020-07-04',
+                    style: TextStyle(color: CupertinoColors.white)),
+                Text('Park Lake',
+                    style: TextStyle(
+                        color: CupertinoColors.white, fontSize: 25.0)),
+              ],
+            ),
+          )),
+    ]);
+
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+          color: CupertinoColors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+                color: CupertinoColors.darkBackgroundGray.withOpacity(.7),
+                blurRadius: 8.0,
+                offset: Offset(0.0, 5.0))
+          ]),
+      child: ClipRRect(borderRadius: BorderRadius.circular(10.0), child: _card),
     );
   }
 }
